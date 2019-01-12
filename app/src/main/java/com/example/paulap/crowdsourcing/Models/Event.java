c2472cb2b154bd7d46c62fe82f8ccff9335d8336
vlad.brincoveanu <gg.vladbrincoveanu@gmail.com>
2019-01-12 15:00:53 +0200

    added models

--- app/src/main/java/com/example/paulap/crowdsourcing/Models/Event.java
+++ app/src/main/java/com/example/paulap/crowdsourcing/Models/Event.java
@@ -1,4 +1,51 @@
 package com.example.paulap.crowdsourcing.Models;
 
+import android.location.Location;
+
+import java.util.Date;
+
 public class Event {
+    private Date data;
+    private Location l;
+    private String goal;
+    private String category;
+
+    public Event(Date data, Location l, String goal, String category) {
+        this.data = data;
+        this.l = l;
+        this.goal = goal;
+        this.category = category;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Location getL() {
+        return l;
+    }
+
+    public void setL(Location l) {
+        this.l = l;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
--- app/src/main/java/com/example/paulap/crowdsourcing/Models/Issue.java
+++ app/src/main/java/com/example/paulap/crowdsourcing/Models/Issue.java
@@ -1,4 +1,47 @@
 package com.example.paulap.crowdsourcing.Models;
 
 public class Issue {
+    private String summary;
+    private String description;
+    private String solution;
+    private String category; // paote fi ENUM sa nu avem mai mult de 3 tipuri predefinite de issues
+
+    public Issue(String summary, String description, String solution, String category) {
+        this.summary = summary;
+        this.description = description;
+        this.solution = solution;
+        this.category = category;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
